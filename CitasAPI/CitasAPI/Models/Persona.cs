bc275be087762cf8a17678dae6e094eb918e8770
DESKTOP-NPF6QST\fox_2 <lcjbeltran1@gmail.com>
2022-06-25 12:12:11 -0600

    push develop

---
+++ CitasAPI/CitasAPI/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CitasAPI.Models
+{
+    public class Persona
+    {
+        public int IdRollElemento { get; set; }
+        public string Nombre { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        public string DireccionElectronica { get; set; }
+        public string img { get; set; }
+    }
+}
---
+++ PlataformaCitas/PlataformaCitas/Models/APIRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace PlataformaCitas.Models
+{
+    public class APIRequest
+    {
+        public lDoctores GetDoctores()
+        {
+            var resp = new lDoctores();
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("https://localhost:44321/api/Doctores");
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                resp = JsonConvert.DeserializeObject<lDoctores>(responseString);
+            }
+            catch (Exception)
+            {
+                resp.bError = true;
+                resp.Descripcion = "Lista de Clientes no Disponible";
+            }
+            return resp;
+        }
+
+        public void TestRequest()
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("https://localhost:44321/api/values");
+                request.Method = "GET";
+                request.ContentType = "application/json";  
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                var resp = JsonConvert.DeserializeObject<List<string>>(responseString);
+            }
+            catch (Exception)
+            {
+
+            }
+            
+        }
+    }
+}
